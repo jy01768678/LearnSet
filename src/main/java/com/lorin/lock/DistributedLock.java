4a77dc03da7b8100ac87bb8e68d2d8717728c763
dongtao <dongtao@55tuan.com>
2014-04-18 16:11:21 +0800

    add lock code

---
+++ src/main/java/com/lorin/lock/DistributedLock.java
@@ -0,0 +1,16 @@
+package com.lorin.lock;
+
+import java.util.concurrent.locks.Lock;
+
+public interface DistributedLock extends Lock{
+
+	Listener getListener();
+	
+	void setListener(Listener listener);
+	
+	interface Listener{
+		void onAbort(DistributedLock lock,Exception e);
+	}
+}
+
+
