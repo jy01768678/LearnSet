573b3c8bc3959829a900f747e8e9bc5328866232
dongtao <dongtao@55tuan.com>
2014-05-30 15:13:59 +0800

    add number al

--- src/main/java/com/lorin/algorithm/art/Find2Num.java
+++ src/main/java/com/lorin/algorithm/art/Find2Num.java
@@ -4,6 +4,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Find2Num {
+	
+	/**
+	 * 寻找水王
+	 */
+	public static int findWaterNum(int[] data){
+		if(data.length == 0){
+			return 0;
+		}
+		int result=data[0];
+		int times = 1;
+		for(int i=1;i<data.length;i++){
+			if(data[i] == result){
+				times++;
+			}else {
+				times--;
+			}
+			if(times == 0){
+				result=data[i];
+				times=1;
+			}
+		}
+		times=0;
+		for(int i=0;i<data.length;i++){
+			if(data[i] == result){
+				times++;
+			}
+		}
+		if(times * 2 <= data.length){
+			//正解了
+		}
+		return result;
+	}
+	
+	/**
+	 * 寻找数组中子数组的最大值
+	 * @param data
+	 * @return
+	 */
+	public static int findMaxSum(int[] data){
+		int max = data[0];
+		int sum = 0;
+		for(int i=0;i<data.length;i++){
+			if(data[i] > 0){
+				sum+=data[i];
+			}else {
+				sum = data[i];
+			}
+			if(sum > max){
+				max = sum;
+			}
+		}
+		return sum;
+	}
 
 	//固定的两个值
 	public static boolean findSum(int[] data,int len,int sum,int firstNum,int secondNum){
