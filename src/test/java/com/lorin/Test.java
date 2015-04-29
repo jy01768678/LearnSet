6df4cd349a0b22734c7a4274a483a4483afbdfa2
董涛 <dongtao@55tuan.com>
2015-04-29 18:10:33 +0800

    add lock

--- src/test/java/com/lorin/Test.java
+++ src/test/java/com/lorin/Test.java
@@ -1,10 +1,21 @@
 package com.lorin;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class Test {
 
 	public static void main(String[] args) {
 //		SafeSingleton singleton = SafeSingleton.INSTANCE;
 //		singleton.println();
 		System.out.printf("%x\n", 31558);
+		AtomicReference<Thread> owner =new AtomicReference<Thread>();
+		AtomicInteger serviceNum = new AtomicInteger();
+		List<String> tst = new ArrayList<String>();
+		tst.add("1");
+		tst.add("2");
+		System.out.println(tst.toString());
 	}
 }
