ed60167b4279220f4cb0013e1052e8afa47d870a
dongtao02 <dongtao02@baidu.com>
2018-02-02 19:34:48 +0800

    update redis address

--- src/main/java/com/lorin/jedis/RedisClient.java
+++ src/main/java/com/lorin/jedis/RedisClient.java
@@ -7,7 +7,7 @@ import redis.clients.jedis.Jedis;
  */
 public class RedisClient {
     public static void main(String[] args) {
-        Jedis jedis = new Jedis("10.94.21.52", 6379);
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
         System.out.print(jedis.get("username"));
     }
 }
